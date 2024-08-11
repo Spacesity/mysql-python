0d488edee5e1f89b4ac14b9d47a4cf79f78853ee
spacesity <spacesity@spacesity.com>
2024-08-11 15:28:46 +0000

    error handling

--- main.py
+++ main.py
@@ -1,67 +1,76 @@
 import mysql.connector
 import getpass
-from mysql.connector import Error
 
 commands = [
-			"list-commands",
-			"create-list",
-			"view-list",
-			"create-task",
-			"remove-task"
-		]
+	"list-commands",
+	"create-list",
+	"view-list",
+	"create-task",
+	"remove-task"
+]
 
 class Command():
 	def view_commands():
-		if command == "list-commands":
+		if command == commands[0]:
 			for com in commands:
 				print(com)
 	def create_list():
-		pass
+		if command == commands[1]:
+			list_name = str(input("Enter new list name:"))
+			list_creation = f"CREATE TABLE {list_name} (task int, description varchar(200))"
+			cursor.execute(list_creation)
 	def view_list():
-		if command == "view-list":
-			cursor.execute("SELECT * FROM scores")
+		if command == commands[2]:
+			list_view = f"SELECT * FROM {database_input}"
+			cursor.execute(list_view)
 			rows = cursor.fetchall()
 			for row in rows:
 				print(row)
 	def create_task():
-		pass
+		if command == commands[3]:
+			pass
 	def remove_task():
-		pass
+		if command == commands[4]:
+			pass
 
-host_input = str(input("Enter host name (localhost if on local device):"))
-user_input = str(input("Enter user name:"))
-password_input = str(getpass.getpass("Enter password:"))
-database_input = str(input("Enter database name:"))
+print(f"MySQL and Python Program - Type '{commands[0]}' to see all commands")
 
-print("MySQL and Python Program - Type 'commands' to see all commands")
-try:
-	conn = mysql.connector.connect(
-		host = host_input,
-		user = user_input,
-		password = password_input,
-		database = database_input
-	)
-	
-	if conn.is_connected():
-		print("Connected to MySQL database")
-		cursor = conn.cursor()
-		while True:
-			command = str(input("execute: ")).strip().lower() 	
-			Command.view_commands()
-			Command.create_list()
-			Command.view_list()
-			Command.create_task()
-			Command.remove_task()
-			if command == "exit":
-				break
-		cursor.close()
+while True:
+	menu = str(input("Login (L) or Exit (E)?: ")).upper()
+	if menu == "L":
+		host_input = str(input("Enter host name (localhost if on local device): "))
+		user_input = str(input("Enter user name: "))
+		password_input = str(getpass.getpass("Enter password: "))
+		database_input = str(input("Enter database name: "))
+		try:
+			conn = mysql.connector.connect(
+				host = host_input,
+				user = user_input,
+				password = password_input,
+				database = database_input
+			)
+			if conn.is_connected():
+				print("Connected to database")
+				cursor = conn.cursor()
+				while True:
+					command = str(input("Execute: ")).strip().lower() 	
+					try:
+						Command.view_commands()
+						Command.create_list()
+						Command.view_list()
+						Command.create_task()
+						Command.remove_task()
+					except mysql.connector.Error as error:
+						print("Error:", error)
+					if command == "exit":
+							break
+				cursor.close()
+				conn.close()
+		except Exception as error:
+			print("Login failed")
+			print("Error:", error)
+	elif menu == "E":
+		print("Goodbye")
+		break
 	else:
-		print("Failed to connect to MySQL database")
-
-except mysql.connector.Error as err:
-	print("Error:", err)
-
-finally:
-	if conn.is_connected():
-		conn.close()
-		print("MySQL connection closed")
+		print("Invalid")
